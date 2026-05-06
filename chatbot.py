15d39756e5fb6fd624a044d9dd233d72384f9c30
chandrasiribezawada-blip <chandrasiribezawada@gmail.com>
2026-05-06 10:54:25 +0530

    Updated chatbot.py for cleaner code

--- chatbot.py
+++ chatbot.py
@@ -2,13 +2,10 @@ from langchain_community.llms import Ollama
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 
-# Load local model (make sure you installed it via ollama pull)
 llm = Ollama(model="llama3")
 
-# Memory
 memory = ConversationBufferMemory()
 
-# Conversation chain
 conversation = ConversationChain(
     llm=llm,
     memory=memory
